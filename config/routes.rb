8a9454aab9fa18a0235989099616b26f513cb429
Yechiel K <chilikkal@gmail.com>
2017-01-30 21:55:16 -0500

    created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+
+  get 'comments/create'
+
+  get 'conversations/index'
+
+  get 'conversations/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'conversations#index'
 
